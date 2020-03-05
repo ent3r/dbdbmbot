e9a0485f577fed7e1ac614121859c8cb435a60c4
Nikolai <32072697+ent3r@users.noreply.github.com>
2020-03-05 20:18:16 +0100

    Add userFromMention and channelFromMention functions

--- functions.js
+++ functions.js
@@ -24,5 +24,33 @@ module.exports = {
   formatDate: function(date) {
     return new Intl.DateTimeFormat("en-US").format(date);
   },
+
+  getUserFromMention: function(client, mention) {
+    // The id is the first and only match found by the RegEx.
+    const matches = mention.match(/^<@!?(\d+)>$/);
+
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return;
+
+    // However the first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
+
+    return client.users.cache.get(id);
+  },
+
+  getChannelFromMention: function(client, mention) {
+    // The id is the first and only match found by the RegEx.
+    const matches = mention.match(/^<#(\d+)>$/);
+    
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return;
+
+    // However the first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
+
+    return client.channels.cache.get(id);
   }
+
 };
