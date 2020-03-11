d060b4f787c248d497d0678fb6402a7af7cccf4e
Nikolai <32072697+ent3r@users.noreply.github.com>
2020-03-11 13:51:57 +0100

    Update onready to work with v12

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
 module.exports.run = (client) => {
     client.user.setPresence({
-      game: {
-        name: "with discord.js"
+      activity: {
+        type: "PLAYING",
+        name: `with discord.js | ${client.my_config.prefix}help`,
+        url: "https://github.com/ent3r/dbdbmbot"
       },
       status: "online"
     });
